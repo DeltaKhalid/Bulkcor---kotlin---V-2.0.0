c8c3cddb020bb82cbe0dcfd7799246d61c60c029
DeltaKhalid <khalid.delta063@gmail.com>
2026-05-16 11:59:31 +0600

    Drawer design has been done

--- app/src/main/java/com/example/bulkcor_app_v_200/DashBoard.kt
+++ app/src/main/java/com/example/bulkcor_app_v_200/DashBoard.kt
@@ -3,7 +3,10 @@ package com.example.bulkcor_app_v_200
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,6 +19,16 @@ class DashBoard : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.dashboardToolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Toggle drawer when left menu icon is tapped (open if closed, close if open)
+        val drawer = findViewById<DrawerLayout?>(R.id.drawer_layout)
+        val ivMenu = findViewById<ImageView?>(R.id.ivMenu)
+        ivMenu?.setOnClickListener {
+            drawer?.let { d ->
+                if (d.isDrawerOpen(GravityCompat.START)) d.closeDrawer(GravityCompat.START)
+                else d.openDrawer(GravityCompat.START)
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
